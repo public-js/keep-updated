6fcd175223d93a4e58343ce4f37ec925139ed76c
Michael M <mullinsmikey@users.noreply.github.com>
2022-02-16 18:26:24 +0400

    chore: package setup

---
+++ packages/keep-updated/src/lib/keep-updated.spec.ts
@@ -0,0 +1,7 @@
+import { keepUpdated } from './keep-updated';
+
+describe('keepUpdated', () => {
+    it('should work', () => {
+        expect(keepUpdated()).toEqual('keep-updated');
+    });
+});
---
+++ packages/keep-updated/src/lib/keep-updated.ts
@@ -0,0 +1,3 @@
+export function keepUpdated(): string {
+    return 'keep-updated';
+}
